a92c9de53920b0c4b10e13e9a6cd9dd1173ac8e3
Ellaluvscheese <gal21029@byui.edu>
2023-10-24 16:13:32 -0600

    lazy load, home large css & more

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,15 +1,22 @@
 function darkMode() {
     const checkBox = document.querySelector("#myCheck");
     const paragraphs = document.querySelectorAll("p");
+    const h3s = document.querySelectorAll("h3");
 
     if (checkBox.checked){
         document.body.style.backgroundColor = 'black';
+        h3s.forEach((h) => {
+            h.style.color = 'white';
+        });
         paragraphs.forEach((p) => {
             p.style.color = 'white';
         });
     }
     else if(checkBox.checked !== true){
         document.body.style.backgroundColor = "";
+        h3s.forEach((h) => {
+            h.style.color = '';
+        });
         paragraphs.forEach((p) => {
             p.style.color = ''; // Set the text color to white
         });
