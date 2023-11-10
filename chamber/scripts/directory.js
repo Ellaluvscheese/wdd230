39781b0daf6999ab119a64cf0f2afab25758b6a2
Ellaluvscheese <gal21029@byui.edu>
2023-11-09 23:14:10 -0700

    prophets & directory

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,40 @@
+//"name": "Sarah",
+//            "address" : "123 Main Street, Rexburg Idaho",
+//            "phone" : "111-111-1111",
+//            "websiteURL" : "https://www.rexburg.org",
+//            "imgeURL" : "images/example.jpg",
+//            "membershipLevel" : "gold"
+
+const url = './data/members.json';
+const cards = document.getElementById('cards');
+
+function displayMembers(members){
+    members.forEach(member => {
+        let section = document.createElement("section");
+        section.classList.add('card');
+        let sectionHTML = `
+            <h2>${member.name}</h2>
+            <p>Phone: ${member.phone}</p>
+            <p>Website: ${member.websiteURL}</p>
+            <img src="${member.imageURL}" alt="Picture of ${member.name}">
+            <p>Membership Level: ${member.membershipLevel}</P>
+        `
+        section.innerHTML = sectionHTML;
+        cards.appendChild(section);
+    });
+};
+
+async function getMemberData() {
+    const response = await fetch(url);
+
+    if (response.ok) {
+        const data = await response.json();
+
+        displayMembers(data.members);
+    }
+    else{
+        console.error("NO WORK")
+    }
+};
+
+getMemberData();
--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -9,19 +9,22 @@
 // "imageurl":
 
 const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
-const cards = document.getElementById(cards);
+const cards = document.getElementById('cards');
 
 function displayProphets(prophets){
     prophets.forEach(prophet => {
-        let section = document.createElement("section")
+        let section = document.createElement("section");
+        section.classList.add('card');
         let sectionHTML = `
             <h2>${prophet.name} ${prophet.lastname}</h2>
             <p>Date of Birth: ${prophet.birthdate}</p>
             <p>Place of Birth: ${prophet.birthplace} </p>
             <img src="${prophet.imageurl}" alt="Picture of the prophet ${prophet.name} ${prophet.lastname}">
         `
+        section.innerHTML = sectionHTML;
+        cards.appendChild(section);
     });
-}
+};
 
 async function getProphetData() {
     const response = await fetch(url);
@@ -34,6 +37,6 @@ async function getProphetData() {
     else{
         console.error("NO WORK")
     }
-}
+};
 
 getProphetData();
