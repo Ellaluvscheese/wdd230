662a454f773088e8068405d6897d21b243d9ece5
Ellaluvscheese <gal21029@byui.edu>
2023-11-20 11:46:04 -0700

    banner & weather js

--- chamber/scripts/meet-greet.js
+++ chamber/scripts/meet-greet.js
@@ -4,12 +4,11 @@ let dayOfWeek = meetDate.getDay();
 if (dayOfWeek == 1 || dayOfWeek == 2 || dayOfWeek == 3){
     let meetGreet = document.getElementById("meetAndGreet");
     meetGreet.style.display = 'block'
-}
+};
 
-let x = document.getElementById("closeMeetAndGreet")
+var x = document.getElementById("closeMeetAndGreet");
 
-if (x.classList.contains('closed')){
-    x.classList.remove('closed')
-} else {
-    
-}
+x.addEventListener("click", function(){
+    let meetGreet = document.getElementById("meetAndGreet");
+    meetGreet.innerHTML = "";
+});
--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,28 +1,73 @@
-const LAT = '39';
-const LON = '-94';
-const APIKEY = "5076daa46c48d89cb2affd9c767ec196";
+// const LAT = '39';
+// const LON = '-94';
+// const APIKEY = "5076daa46c48d89cb2affd9c767ec196";
 
-const APIURL2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${LAT}&lon=&${LON}&appid=${APIKEY}`
-
-
-function showWindChill(temp, speed){
-    const windchillSpan = document.getElementById("windChill");
-
-    let message = "N/A";
-
-    if (temp <= 50 && speed > 3){
-        let chillFactor = Math.pow(speed, 0.16);
-        let chill = 35.74 + (0.6215 * temp) - (35.75*chillFactor) + (0.4275 * temp * chillFactor)
-        message = `${chill}`;
-    }
+const apiURL = `https://api.openweathermap.org/data/2.5/forecast?lat=39&lon=-94&appid=5076daa46c48d89cb2affd9c767ec196&units=imperial`
 
 
-    windchillSpan.textContent = message;
+function displayWeatherData(wData){
+    const icon = `https://openweathermap.org/img/wn/${wData.list[0].weather[0].icon}@2x.png`
+    const description = wData.list[0].weather[0].description;
+    const tempCurr = wData.list[0].main.temp.toFixed();
+    const tempNext = wData.list[1].main.temp.toFixed();
+    const tempNext2 = wData.list[2].main.temp.toFixed();
+    const tempNext3 = wData.list[4].main.temp.toFixed();
+
+    let weatherIcon = document.getElementById("weatherIcon");
+    weatherIcon.setAttribute('src', icon);
+    weatherIcon.setAttribute('alt', description);
+
+    let descriptionWeather = document.getElementById("description");
+    descriptionWeather.innerHTML = `${description}`;
+
+    let currTemp = document.getElementById('temp');
+    currTemp.innerHTML = `${tempCurr}&deg;F`;
+
+    let nextTemp = document.getElementById("nextTemp");
+    nextTemp.innerHTML = `${tempNext}&deg;F`;
+
+    let nextTemp2 = document.getElementById("nextTemp2");
+    nextTemp2.innerHTML = `${tempNext2}&deg;F`;
+
+    let nextTemp3 = document.getElementById("nextTemp3");
+    nextTemp3.innerHTML = `${tempNext3}&deg;F`;
 }
 
-const temperatureSpan = document.getElementById("temperature");
-const windSpeedSpan = document.getElementById("windSpeed");
-const temperature = parseInt(temperatureSpan.textContent);
-const windSpeed = parseInt(windSpeedSpan.textContent);
+async function getWeather(){
+    try{
+        const response = await fetch(apiURL);
+        if(response.ok){
+            const data = await response.json();
+            displayWeatherData(data);
+        }else{
+            throw Error(await response.text());
+        }
+    }catch (error){
+        console.log(error);
+    }
+}
 
-showWindChill(temperature, windSpeed)
+getWeather();
+
+
+// function showWindChill(temp, speed){
+//     const windchillSpan = document.getElementById("windChill");
+
+//     let message = "N/A";
+
+//     if (temp <= 50 && speed > 3){
+//         let chillFactor = Math.pow(speed, 0.16);
+//         let chill = 35.74 + (0.6215 * temp) - (35.75 * chillFactor) + (0.4275 * temp * chillFactor)
+//         message = `${chill}`;
+//     }
+
+
+//     windchillSpan.textContent = message;
+// }
+
+// const temperatureSpan = document.getElementById("temperature");
+// const windSpeedSpan = document.getElementById("windSpeed");
+// const temperature = parseInt(temperatureSpan.textContent);
+// const windSpeed = parseInt(windSpeedSpan.textContent);
+
+// showWindChill(temperature, windSpeed)
