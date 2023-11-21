5f83094a304c4415e8fc875716a2b504c199e4b4
Ellaluvscheese <gal21029@byui.edu>
2023-11-21 13:52:53 -0700

    home button & thank you html fixed

--- chamber/scripts/meet-greet.js
+++ chamber/scripts/meet-greet.js
@@ -1,14 +1,31 @@
 let meetDate = new Date();
 let dayOfWeek = meetDate.getDay();
-
+let menu = document.getElementById("menu");
+let x = document.getElementById("closeMeetAndGreet");
 if (dayOfWeek == 1 || dayOfWeek == 2 || dayOfWeek == 3){
     let meetGreet = document.getElementById("meetAndGreet");
-    meetGreet.style.display = 'block'
+
+    meetGreet.style.display = 'block';
+    // if (menu.classList.contains('opened')){
+    //     x.style.position = "absolute"
+    //     x.style.top = "45%"
+    // }
+
 };
 
-var x = document.getElementById("closeMeetAndGreet");
 
 x.addEventListener("click", function(){
     let meetGreet = document.getElementById("meetAndGreet");
     meetGreet.style.display = 'none';
 });
+
+function fixX(){
+    let menu = document.getElementById("menu");
+    if (menu.classList.contains('opened')){
+        menu.classList.remove('opened');
+    }else{
+        menu.classList.add('opened')
+    }
+}
+
+menu.addEventListener("click", fixX)
