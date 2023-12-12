1d49913bf4bc2be3f7e1c919b52328738f8a077c
Ellaluvscheese <gal21029@byui.edu>
2023-12-12 13:13:14 -0700

    form rules set

--- fruit/scripts/order.js
+++ fruit/scripts/order.js
@@ -1,30 +1,54 @@
-const fruitDiv = document.querySelector(".divChecks")
+const fruitDiv = document.querySelector(".divChecks");
 const url = './data/fruit.json';
+const submitButton = document.getElementById("submitButton")
 
-function displayFruit(data){
+function displayFruit(data) {
     data.forEach(f => {
         let check = document.createElement("label");
         check.classList.add("radioLabel");
         let checkHTML = `
-        <input class='deezNuts' type="checkbox" name="${f.name}" id="${f.id}">
+        <input class='deezNuts' type="checkbox" name="checkboxGroup" value="${f.name}" id="${f.id}">
         ${f.name}
-        `
+        `;
         check.innerHTML = checkHTML;
         fruitDiv.appendChild(check);
     });
 }
 
-async function getFruitData(){
+async function getFruitData() {
     const response = await fetch(url);
 
     if (response.ok) {
         const data = await response.json();
-
         displayFruit(data);
-    }
-    else{
-        console.error("NO WORK")
+    } else {
+        console.error("NO WORK");
     }
 }
 
-getFruitData();
+function checkBoxesSlay() {
+    const checkboxes = document.querySelectorAll('input[name="checkboxGroup"]');
+    var maxChecks = 3;
+
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', () => {
+            var curr = document.querySelectorAll('input[name="checkboxGroup"]:checked').length;
+            checkboxes.forEach(cb => {
+                cb.disabled = curr >= maxChecks && !cb.checked;
+            });
+
+            if (curr === 3){
+                submitButton.type = 'submit';
+            } else {
+                submitButton.type = "button";
+            }
+        });
+    });
+}
+
+async function loadDataAndInitialize() {
+    await getFruitData();
+    checkBoxesSlay();
+}
+
+loadDataAndInitialize();
